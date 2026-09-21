50adcafbbcbc5e01c1b9ac4a68f754b8c610dfc1
Frederico Truocchio <128758607+fredericotru@users.noreply.github.com>
2026-09-21 23:02:55 +0000

    chore:cria a estrutura principal do projeto

---
+++ src/main/java/equipe/calc.java
@@ -0,0 +1,19 @@
+package equipe;
+
+public class Calc {
+
+    // Pessoa A
+    public int soma(int a, int b) {
+        return 0;
+    }
+
+    // Pessoa B
+    public int subtracao(int a, int b) {
+        return 0;
+    }
+
+    // Pessoa C
+    public int multiplicacao(int a, int b) {
+        return 0;
+    }
+}
